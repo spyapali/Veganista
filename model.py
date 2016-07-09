c3c096adf2235c496fd1143092e5a14b5dd63d17
SHALINI PYAPALI <spyapali@SHALINIs-MacBook-Pro.local>
2016-07-09 00:02:28 -0700

    modified user file

--- model.py
+++ model.py
@@ -125,6 +125,7 @@ def connect_to_db(app, db_uri=None):
 
     # Configure to use our PstgreSQL database
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri or 'postgresql:///nutrition'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environment.get("DATABASE_URL", 'postgresql:///nutrition')
     app.config['SQLALCHEMY_ECHO'] = False
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
