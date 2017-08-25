361f85bcfd7906f584652dafe1b3058ecf74f89f
Shalini Pyapali <shalu.pyapali@gmail.com>
2017-08-24 23:42:12 -0700

    Change ENV variable names in send_sms.

--- send_sms.py
+++ send_sms.py
@@ -3,15 +3,15 @@ import os
 from twilio.rest import Client
 
 
-account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-account_token = os.environ["TWILIO_AUTH_TOKEN"]
+account_sid = os.environ["USER_ACCOUNT_SID"]
+account_token = os.environ["USER_AUTH_TOKEN"]
 
 
-client = Client(account_SID)
+client = Client(account_sid, account_token)
 
 client.messages.create(
-    to=os.environ["USER_PHONE_NUMBER"],
-    from_="+14087419974",
+    to=os.environ["MY_NUMBER"],
+    from_=os.environ["TWILIO_NUMBER"],
     body="Good morning! Hope you are ready to log in dishes for the day."
 
 )
